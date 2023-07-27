25c1fb37837b1e6a097c8976bd6e26abccae174a
Warren Humphreys <warrenhumphreys@gmail.com>
2023-07-27 15:45:55 +0100

    Add files via upload

---
+++ AdminCarsalesdashboardmaster/CARSALESADMIN_DASH_APP/DB/CarFeature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewDesignTrial.Models.DB
+{
+    public partial class CarFeature
+    {
+        public CarFeature()
+        {
+            Cars = new HashSet<IndividualCar>();
+        }
+
+        public int FeatureId { get; set; }
+        public string? Description { get; set; }
+
+        public virtual ICollection<IndividualCar> Cars { get; set; }
+    }
+}
---
+++ AdminCarsalesdashboardmaster/CARSALESADMIN_DASH_APP/DB/CarRentalsWithCustomerName.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewDesignTrial.Models.DB
+{
+    public class CarRentalsWithCustomerName
+    {
+        [Key]
+        public int RentalId { get; set; }
+        public string RegistrationNumber { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string RentDate { get; set; }
+        
+        public string ReturnDueDate { get; set; }
+
+        private string returnDate;
+        public string ReturnDate
+        {
+            get { return returnDate; }
+            set
+            {
+                if(value=="01/01/0001")
+                {
+                    this.returnDate = null;
+                }
+                else
+                {
+                    this.returnDate = value;
+                }
+            }
+        
+        }
+        public string TotalPrice { get; set; }
+    }
+}
